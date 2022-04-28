05a9874dc77537c927b67d2b5b05bb2f414fc38c
susa.keyber <susa@keyber.jp>
2022-04-29 07:19:23 +0900

    コメントつけた

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_sample_dartdoc/flutter_sample_dartdoc.dart';
 
@@ -30,8 +30,8 @@ class _MyAppState extends State<MyApp> {
     // Platform messages may fail, so we use a try/catch PlatformException.
     // We also handle the message potentially returning null.
     try {
-      platformVersion =
-          await FlutterSampleDartdoc.platformVersion ?? 'Unknown platform version';
+      platformVersion = await FlutterSampleDartdoc.platformVersion ??
+          'Unknown platform version';
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
--- lib/flutter_sample_dartdoc.dart
+++ lib/flutter_sample_dartdoc.dart
@@ -1,11 +1,24 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+/// クラスの説明の概要です。
+///
+/// ここに詳細を記載していきます。文章が長くなったらこんな感じに
+/// 改行をしていくとよいです。1回の改行では改行になりません。
+///
+/// 2回改行をいれると改行になります。
 class FlutterSampleDartdoc {
   static const MethodChannel _channel = MethodChannel('flutter_sample_dartdoc');
 
+  /// メソッドの説明
+  ///
+  /// サンプルコードがあるとよいです。
+  /// ```dart
+  /// final version = await FlutterSampleDartdoc.platformVersion ?? 'Unknown platform version';
+  /// ```
+  ///
+  /// [FlutterSampleDartdoc] のように [] をつけて強調表示するとうれしいです。
   static Future<String?> get platformVersion async {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
